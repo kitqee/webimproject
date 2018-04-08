e143d187e99ec4868ae7dd2d9e0e00ca710f2c80
aesfur <aesfur@gmail.com>
2018-04-08 15:55:54 +0300

    4

--- webimproject/settings.py
+++ webimproject/settings.py
@@ -131,7 +131,3 @@ USE_TZ = True
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
