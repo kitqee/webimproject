aa99667046448a324a675a25b41b16e62760d2b3
aesfur <aesfur@gmail.com>
2018-04-08 15:54:40 +0300

    3

--- webimproject/urls.py
+++ webimproject/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^auth/', include('apps.authenticate.urls')),
     url(r'^', include('apps.webimtest.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
